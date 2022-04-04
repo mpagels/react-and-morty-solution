6d14dd5999ba7eb93c7cf800131a2395053af47f
Martin <office@martinpagels.de>
2022-04-04 23:16:48 +0200

    add simple markup for startpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import './App.css'
+
 function App() {
-  return <div className="App">Hello world</div>
+  return (
+    <div className="App">
+      <header>React and Morty</header>
+      <main>
+        <ul>
+          <li>
+            <img
+              src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+              alt="rick-and-morty-character"
+            />
+            <h2>Morty Smith</h2>
+            <button>show more</button>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <nav>
+          <ul>
+            <li>Homepage</li>
+            <li>Favorite</li>
+            <li>Random</li>
+            <li>Other</li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  )
 }
 
 export default App
